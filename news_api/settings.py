f1b52a0575d3caadb83fda452f755eb0c0a22ae7
Marcos Montes <82398199+ParZivalT99@users.noreply.github.com>
2022-09-15 16:04:05 -0500

    Update settings.py

--- news_api/settings.py
+++ news_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mit&=ddg5q0)=d7k-@h2q03(&o*8x5w3g%!kmi1wu2^v9&0=-a"
+SECRET_KEY = "YOUR_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
